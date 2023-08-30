e535a49e3e82d351f073ea20d238fc60ce2cc718
@FF7FS <120066595+eooe31@users.noreply.github.com>
2023-08-30 17:42:51 +0300

    Add files via upload

--- checktele.py
+++ checktele.py
@@ -43,13 +43,13 @@ def check_user(username):
 def gen_user(choice):
     if choice == "1":
         c = random.choices(a)
-        d = random.choices(e)
+        d = random.choices(a)
         s = random.choices(e)
         f = [c[0], "_", d[0], "_", s[0]]
         username = ''.join(f)
         if username in banned[0]:
             c = random.choices(a)
-            d = random.choices(e)
+            d = random.choices(b)
             s = random.choices(e)
             f = [c[0], "_", d[0], "_", s[0]]
             username = ''.join(f)
@@ -64,7 +64,7 @@ def gen_user(choice):
         if username in banned[0]:
             c = random.choices(a)
             d = random.choices(e)
-            s = random.choices(e)
+            s = random.choices(b)
             f = [c[0], "_", d[0], "_", s[0]]
             username = ''.join(f)
         else:
@@ -97,7 +97,7 @@ def gen_user(choice):
         username = username+'bot'
         if username in banned[0]:
             c = random.choices(a)
-            d = random.choices(e)
+            d = random.choices(b)
             s = random.choices(e)
             f = [c[0], s[0], d[0]]
             random.shuffle(f)
